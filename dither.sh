cf25b130b00b1da4a091891ef1ba9c68dc22b7ec
Carlana Johnson <mail@carlana.net>
2024-10-25 12:24:00 -0400

    Init

---
+++ dither.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+# Get the directory that this script file is in
+THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+cd "$THIS_DIR"
+
+IN="$1"
+AMOUNT="${2:-50}"
+OUT="${IN%.*}.cga.png"
+
+magick "$IN" \
+	-resize x200 \
+	-define dither:diffusion-amount="$AMOUNT"% \
+	-define dither:diffusion-level=8,8,8,8,8,8 \
+	-dither FloydSteinberg \
+	-remap cga-palette.png \
+	-filter Point \
+	-resize 400% \
+	"$OUT"
+
+open -R "$OUT"
